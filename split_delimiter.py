af8eeca9d01a708ec49438033f9dff8a9a11ac41
Leifowitsch <leifreinhold2@gmail.com>
2026-05-03 17:54:25 +0200

    testing

--- split_delimiter.py
+++ split_delimiter.py
@@ -29,32 +29,6 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                         italic = False
                         split = index
 
-                case delimiter, TextType.code:
-                    if code is not True:
-                        new_nodes.append(TextNode(node.value[split:index], text_type=TextType.plain))
-                        split = index
-                        code = True
-                    elif code is True:
-                        new_nodes.append(TextNode(value=node.value[split+1:index], text_type=TextType.code))
-                        code = False
-                        split = index
-
-                case (delimiter,TextType.bold):
-                    if bold is not True:
-                        new_nodes.append(TextNode(node.value[split:index], text_type=TextType.plain))
-                        split = index
-                        bold = True
-                    elif bold is True:
-                        new_nodes.append(TextNode(value=node.value[split+1:index], text_type=TextType.bold))
-                        bold = False
-                        split = index
-            # Wenn der Index den letzten char erreicht hat und split nicht gleich dem Index ist bedeutet das, dass der letzte teil des Textes plain sein muss und split ist entweder der anfang weil es nie gesplitet wurde
-            # oder split ist der letzte punkt wo der Text gesplitet wurde. also wo die schriftart aufgehört hat 
-            if index == ende and split != index:
-                new_nodes.append(TextNode(node.value[split:index], text_type=TextType.plain)) 
-# du bist seeehr süß
-
-
 
 
 
