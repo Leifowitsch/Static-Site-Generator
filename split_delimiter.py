2c472b287209e66583ba34b720a26a9a616f5f3a
Leifowitsch <leifreinhold2@gmail.com>
2026-05-04 16:28:11 +0200

    Logik kürzer geamcht von split nodes

--- split_delimiter.py
+++ split_delimiter.py
@@ -8,26 +8,28 @@ class Delimiters(Enum):
      
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    text_art = text_type
     if text_type == TextType.plain:
         return old_nodes
     new_nodes = []
     for node in old_nodes:
-        italic =False
-        bold = False
-        code = False
-        split = 0
+        if node.text_type != TextType.plain:
+            new_nodes.append(node)
+        anfang = True
+        split_punkt = 0
         ende = len(node.value) -1
         for index in range(len(node.value)):
-            match (node.value[index], text_type):
-                case (delimiter, TextType.italic):
-                    if italic is not True:
-                        new_nodes.append(TextNode(node.value[split:index], text_type=TextType.plain))
-                        split = index
-                        italic = True
-                    elif italic is True:
-                        new_nodes.append(TextNode(value=node.value[split+1:index], text_type=TextType.italic))
-                        italic = False
-                        split = index
+            match (node.value[index]):
+                case (delimiter):
+                    if anfang is True:
+                        new_nodes.append(TextNode(node.value[split_punkt:index], text_type=TextType.plain))
+                        split_punkt = index
+                        anfang = False
+                    elif anfang is not True:
+                        new_nodes.append(TextNode(value=node.value[split_punkt+1:index], text_type=text_art))
+                        split_punkt = index
+                        anfang = True
+            
 
 
 
